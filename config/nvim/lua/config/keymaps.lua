c629fb18919e4d96a954b4e6846c1f06c08f1c80
Brian LaMattina <brian@lamattina.io>
2025-11-02 08:30:30 -0500

    Add terminal mode navigation keymaps
    Add Ctrl-[hjkl] keymaps to navigate between splits from terminal mode,
    automatically exiting terminal mode when moving to another split. Also
    add Esc-Esc shortcut to exit terminal mode to normal mode.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,15 @@ keymap.set("n", "<C-j>", "<Cmd>wincmd j<CR>", { silent = true, desc = "Move to t
 keymap.set("n", "<C-h>", "<Cmd>wincmd h<CR>", { silent = true, desc = "Move to the left split" })
 keymap.set("n", "<C-l>", "<Cmd>wincmd l<CR>", { silent = true, desc = "Move to the right split" })
 
+-- Use Ctrl-[hjkl] to navigate out of terminal mode
+keymap.set("t", "<C-k>", "<C-\\><C-n><Cmd>wincmd k<CR>", { silent = true, desc = "Exit terminal and move to above split" })
+keymap.set("t", "<C-j>", "<C-\\><C-n><Cmd>wincmd j<CR>", { silent = true, desc = "Exit terminal and move to below split" })
+keymap.set("t", "<C-h>", "<C-\\><C-n><Cmd>wincmd h<CR>", { silent = true, desc = "Exit terminal and move to left split" })
+keymap.set("t", "<C-l>", "<C-\\><C-n><Cmd>wincmd l<CR>", { silent = true, desc = "Exit terminal and move to right split" })
+
+-- Easy escape from terminal mode to normal mode
+keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
+
 -- Search on highlighted text in visual mode with "//"
 keymap.set("v", "//", 'y/<C-R>"<CR>', { noremap = true, silent = true, desc = "Search highlighted text" })
 
